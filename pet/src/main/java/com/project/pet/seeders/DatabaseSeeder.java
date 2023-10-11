542474ece7ace98e8fdb7d91e4fec7b50ef28d7e
Gustavo França <gstvz.franca@gmail.com>
2023-10-11 18:15:20 -0300

    feat: implemented DatabaseSeeder

---
+++ pet/src/main/java/com/project/pet/seeders/DatabaseSeeder.java
@@ -0,0 +1,39 @@
+package com.project.pet.seeders;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.project.pet.dto.Usuario.UsuarioDTO;
+import com.project.pet.model.UserTipo;
+import com.project.pet.model.Usuario;
+import com.project.pet.repository.UsuarioRepository;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		seedUsuarios();
+	}
+
+	private void seedUsuarios() {
+		if (usuarioRepository.count() == 0) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate dataNascimento = LocalDate.parse("2012-04-23", formatter);			
+			
+			UsuarioDTO usuarioDTO = new UsuarioDTO("Admin", "999.999.999-99", dataNascimento, "(99) 99999-9999", UserTipo.FUNCIONARIO, "teste@email.com", "Admin@123");
+            Usuario usuarioSeed = new Usuario(usuarioDTO);
+            usuarioSeed.setSenha(new BCryptPasswordEncoder().encode(usuarioDTO.senha()));
+            
+            usuarioRepository.save(usuarioSeed);
+		}
+	}
+}
