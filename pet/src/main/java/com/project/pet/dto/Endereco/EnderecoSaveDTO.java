4ae711767f45ec291c450e12dccd0385cb3cf471
Gustavo França <gstvz.franca@gmail.com>
2023-10-11 14:50:50 -0300

    feat: created EnderecoSaveDTO; Added cep to Endereco entity and convertToEntity method

---
+++ pet/src/main/java/com/project/pet/dto/Endereco/EnderecoSaveDTO.java
@@ -0,0 +1,4 @@
+package com.project.pet.dto.Endereco;
+
+public record EnderecoSaveDTO(String logradouro, String bairro, String cidade, String estado, String cep) {
+}
--- pet/src/main/java/com/project/pet/model/Endereco.java
+++ pet/src/main/java/com/project/pet/model/Endereco.java
@@ -1,5 +1,7 @@
 package com.project.pet.model;
 
+import com.project.pet.dto.Endereco.EnderecoSaveDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,51 +10,79 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="enderecos")
+@Table()
 public class Endereco {
-
-	 @Id
-	 @GeneratedValue(strategy = GenerationType.AUTO)
-	 private Integer idEndereco;
-	 @Column(length = 255)
-	 private String logradouro;
-	 @Column(length = 255)
-	 private String cidade;
-	 @Column(length = 100)
-	 private String bairro;
-	 @Column(length = 2)
-	 private String estado;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer idEndereco;
+	@Column(length = 255)
+	private String logradouro;
+	@Column(length = 255)
+	private String cidade;
+	@Column(length = 100)
+	private String bairro;
+	@Column(length = 2)
+	private String estado;
+	@Column(length = 9)
+	private String cep;
+	
+	public Endereco(String logradouro, String cidade, String bairro, String estado, String cep) {
+		this.logradouro = logradouro;
+		this.cidade = cidade;
+		this.bairro = bairro;
+		this.estado = estado;
+		this.cep = cep;
+	}
+	 
 	public Integer getIdEndereco() {
 		return idEndereco;
 	}
+	
 	public void setIdEndereco(Integer idEndereco) {
 		this.idEndereco = idEndereco;
 	}
+	
 	public String getLogradouro() {
 		return logradouro;
 	}
+	
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
+	
 	public String getCidade() {
 		return cidade;
 	}
+	
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+	
 	public String getBairro() {
 		return bairro;
 	}
+	
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+	
 	public String getEstado() {
 		return estado;
 	}
+	
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-	 
-	 
 	
+	public String getCep() {
+		return cep;
+	}
+	
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	 
+    public static Endereco convertToEntity(EnderecoSaveDTO enderecoSaveDTO) {
+        return new Endereco(enderecoSaveDTO.logradouro(), enderecoSaveDTO.cidade(), enderecoSaveDTO.bairro(), enderecoSaveDTO.estado(), enderecoSaveDTO.cep());
+    }	
 }
