75c4c8731c3838e87a4dfb6cd3951faabdd8db0b
Gustavo França <gstvz.franca@gmail.com>
2023-10-11 11:27:23 -0300

    feat: changed register response status to 201

--- pet/src/main/java/com/project/pet/controller/AuthenticationController.java
+++ pet/src/main/java/com/project/pet/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.project.pet.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -48,7 +49,7 @@ public class AuthenticationController {
 
         this.repository.save(newUser);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
     
 }
