eb745bae738ec2e447a8aeeff5d798f582ee96b3
Gustavo França <gstvz.franca@gmail.com>
2023-10-12 09:22:20 -0300

    fix: fixed UsuarioFindAllDTO props order

--- pet/src/main/java/com/project/pet/dto/Usuario/UsuarioFindAllDTO.java
+++ pet/src/main/java/com/project/pet/dto/Usuario/UsuarioFindAllDTO.java
@@ -2,5 +2,5 @@ package com.project.pet.dto.Usuario;
 
 import com.project.pet.model.UserTipo;
 
-public record UsuarioFindAllDTO(Integer id, String cpf, String nome, String email, UserTipo tipo) {
+public record UsuarioFindAllDTO(Integer id, String nome, String cpf, String email, UserTipo tipo) {
 }
