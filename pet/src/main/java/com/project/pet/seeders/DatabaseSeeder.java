85cbe4f5e705dddbf4c1e07ddaf56f46c1824cc2
Gustavo França <gstvz.franca@gmail.com>
2023-10-22 14:46:46 -0300

    chore: renamed dataNascimento to data_nascimento to mantain a pattern

--- pet/src/main/java/com/project/pet/seeders/DatabaseSeeder.java
+++ pet/src/main/java/com/project/pet/seeders/DatabaseSeeder.java
@@ -34,9 +34,9 @@ public class DatabaseSeeder implements CommandLineRunner {
     private void seedUsuarios() {
         if (usuarioRepository.count() == 0) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDate dataNascimento = LocalDate.parse("2000-01-01", formatter);
+            LocalDate data_nascimento = LocalDate.parse("2000-01-01", formatter);
 
-            UsuarioSaveDTO usuarioSaveDTO = new UsuarioSaveDTO("Admin", "999.999.999-99", dataNascimento, "(99) 99999-9999", UserTipo.FUNCIONARIO, "admin@email.com", "Admin@123");
+            UsuarioSaveDTO usuarioSaveDTO = new UsuarioSaveDTO("Admin", "999.999.999-99", data_nascimento, "(99) 99999-9999", UserTipo.FUNCIONARIO, "admin@email.com", "Admin@123");
             Usuario usuarioSeed = new Usuario(usuarioSaveDTO);
             usuarioSeed.setSenha(new BCryptPasswordEncoder().encode(usuarioSaveDTO.senha()));
 
@@ -47,12 +47,12 @@ public class DatabaseSeeder implements CommandLineRunner {
     private void seedAnimais() {
         if (animalRepository.count() == 0) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDate dataNascimento = LocalDate.parse("2000-01-01", formatter);
+            LocalDate data_nascimento = LocalDate.parse("2000-01-01", formatter);
 
-            UsuarioSaveWithIdDTO usuario = new UsuarioSaveWithIdDTO(1, "Admin", "999.999.999-99", dataNascimento, "(99) 99999-9999", UserTipo.FUNCIONARIO, "admin@email.com", "Admin@123");
+            UsuarioSaveWithIdDTO usuario = new UsuarioSaveWithIdDTO(1, "Admin", "999.999.999-99", data_nascimento, "(99) 99999-9999", UserTipo.FUNCIONARIO, "admin@email.com", "Admin@123");
             Usuario usuarioSeed = new Usuario(usuario);
 
-            Animal animalSeed = new Animal(1, "Animal", dataNascimento, "especie", "raca", AnimalTipo.MASCULINO, "porte", "pelagem", "cor", 5.5, usuarioSeed);
+            Animal animalSeed = new Animal(1, "Animal", data_nascimento, "especie", "raca", AnimalTipo.MASCULINO, "porte", "pelagem", "cor", 5.5, usuarioSeed);
 
             animalRepository.save(animalSeed);
         }
