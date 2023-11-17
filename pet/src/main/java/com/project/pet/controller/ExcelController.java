46baddb006b3d23f5fd1bbbe58bdd88983bccfb6
MarianaMancan <80931170+MarianaMancan@users.noreply.github.com>
2023-11-16 22:40:30 -0300

    Update ExcelController.java

--- pet/src/main/java/com/project/pet/controller/ExcelController.java
+++ pet/src/main/java/com/project/pet/controller/ExcelController.java
@@ -3,10 +3,12 @@ package com.project.pet.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import com.project.pet.dto.Animal.AnimalFindAllDTO;
 import com.project.pet.dto.Usuario.UsuarioFindAllDTO;
@@ -15,120 +17,60 @@ import com.project.pet.service.AnimalService;
 import com.project.pet.service.ExcelService;
 import com.project.pet.service.UsuarioService;
 import com.project.pet.service.VacinacaoService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
-@Service
-public class ExcelService {
-	
-	  public byte[] criarArquivoExcelVacinacoes(final String nomeArquivo, List<VacinacaoFindAllDTO> vacinacoes) {
-	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
 
-	        try (var workbook = new XSSFWorkbook(); var outputStream = new ByteArrayOutputStream()) {
-	            var planilha = workbook.createSheet("Lista de Vacinações");
-	            int numeroDaLinha = 0;
 
-	            adicionarCabecalhoVacinacao(planilha, numeroDaLinha++);
+@RestController
+public class ExcelController {
+	@Autowired
+	private ExcelService excelService;
+	@Autowired
+	private VacinacaoService vacinacaoService;
+	@Autowired
+	private AnimalService animalService;
+	@Autowired
+	private UsuarioService usuarioService;
+	 @GetMapping("/export/relatorioVacinacao")
+	    public ResponseEntity<ByteArrayResource> exportVacinacaoToExcel() {
+	        List<VacinacaoFindAllDTO> vacinacoes = vacinacaoService.fetchVacinacaoList();
+	        byte[] excelBytes = excelService.criarArquivoExcelVacinacoes("vacinacoes.xls", vacinacoes);
 
-	            for (VacinacaoFindAllDTO vacinacao : vacinacoes) {
-	                var linha = planilha.createRow(numeroDaLinha++);
-	                adicionarCelula(linha, 0, vacinacao.id());
-	                adicionarCelula(linha, 1, vacinacao.animal_nome());
-	                adicionarCelula(linha, 2, vacinacao.descricao());
-	                adicionarCelula(linha, 3, String.valueOf(vacinacao.status()));
-	                adicionarCelula(linha, 4, vacinacao.data_vacinacao().format(formatter));
-	            }
+	        ByteArrayResource resource = new ByteArrayResource(excelBytes);
 
-	            workbook.write(outputStream);
-	            return outputStream.toByteArray();
-	        } catch (IOException e) {
-	            System.err.println("Erro ao processar o arquivo: " + nomeArquivo);
-	            return new byte[0];
-	        }
+	        return ResponseEntity.ok()
+	                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=vacinacoes.xls")
+	                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+	                .contentLength(excelBytes.length)
+	                .body(resource);
 	    }
 
-	
-	    public byte[] criarArquivoExcelAnimal(final String nomeArquivo, List<AnimalFindAllDTO> animais) {
-	        try (var workbook = new XSSFWorkbook(); var outputStream = new ByteArrayOutputStream()) {
-	            var planilha = workbook.createSheet("Lista de Animais");
-	            int numeroDaLinha = 0;
+	    @GetMapping("/export/relatorioAnimal")
+	    public ResponseEntity<ByteArrayResource> exportAnimalToExcel() {
+	        List<AnimalFindAllDTO> animais = animalService.fetchAnimalList();
+	        byte[] excelBytes = excelService.criarArquivoExcelAnimal("animais.xls", animais);
 
-	            adicionarCabecalhoAnimal(planilha, numeroDaLinha++);
+	        ByteArrayResource resource = new ByteArrayResource(excelBytes);
 
-	            for (AnimalFindAllDTO animal : animais) {
-	                var linha = planilha.createRow(numeroDaLinha++);
-	                adicionarCelula(linha, 0, animal.id());
-	                adicionarCelula(linha, 1, animal.nome());
-	                adicionarCelula(linha, 2, animal.especie());
-	                adicionarCelula(linha, 3, animal.raca());
-	                adicionarCelula(linha, 4, animal.tutor_nome());
-	            }
-
-	            workbook.write(outputStream);
-	            return outputStream.toByteArray();
-	        } catch (IOException e) {
-	            System.err.println("Erro ao processar o arquivo: " + nomeArquivo);
-	            return new byte[0];
-	        }
+	        return ResponseEntity.ok()
+	                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=animais.xls")
+	                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+	                .contentLength(excelBytes.length)
+	                .body(resource);
 	    }
 
-	
-	
-	    public byte[] criarArquivoExcelUsuario(final String nomeArquivo, List<UsuarioFindAllDTO> usuarios) {
-	        try (var workbook = new XSSFWorkbook(); var outputStream = new ByteArrayOutputStream()) {
-	            var planilha = workbook.createSheet("Lista de Usuarios");
-	            int numeroDaLinha = 0;
+	    @GetMapping("/export/relatorioUsuario")
+	    public ResponseEntity<ByteArrayResource> exportUsuarioToCsv() {
+	        List<UsuarioFindAllDTO> usuarios = usuarioService.fetchUsuarioList();
+	        byte[] excelBytes = excelService.criarArquivoExcelUsuario("usuarios.xls", usuarios);
 
-	            adicionarCabecalhoUsuario(planilha, numeroDaLinha++);
+	        ByteArrayResource resource = new ByteArrayResource(excelBytes);
 
-	            for (UsuarioFindAllDTO usuario : usuarios) {
-	                var linha = planilha.createRow(numeroDaLinha++);
-	                adicionarCelula(linha, 0, usuario.id());
-	                adicionarCelula(linha, 1, usuario.nome());
-	                adicionarCelula(linha, 2, usuario.email());
-	                adicionarCelula(linha, 3, usuario.cpf());
-	                adicionarCelula(linha, 4, String.valueOf(usuario.tipo()));
-	            }
-
-	            workbook.write(outputStream);
-	            return outputStream.toByteArray();
-	        } catch (IOException e) {
-	            System.err.println("Erro ao processar o arquivo: " + nomeArquivo);
-	            return new byte[0];
-	        }
+	        return ResponseEntity.ok()
+	                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=usuarios.xls")
+	                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+	                .contentLength(excelBytes.length)
+	                .body(resource);
 	    }
-
-	private void adicionarCabecalhoVacinacao(XSSFSheet planilha, int numeroLinha) {
-		var linha = planilha.createRow(numeroLinha);
-		adicionarCelula(linha, 0, "Id");
-		adicionarCelula(linha, 1, "Nome animal");
-		adicionarCelula(linha, 2, "Descrição");
-		adicionarCelula(linha, 3, "Status");
-		adicionarCelula(linha, 4, "DataVacinacao");
 	}
-	private void adicionarCabecalhoAnimal(XSSFSheet planilha, int numeroLinha) {
-		var linha = planilha.createRow(numeroLinha);
-		adicionarCelula(linha, 0, "Id");
-		adicionarCelula(linha, 1, "Nome animal");
-		adicionarCelula(linha, 2, "Espécie");
-		adicionarCelula(linha, 3, "Raca");
-		adicionarCelula(linha, 4, "Nome tutor");
-	}
-	
-	private void adicionarCabecalhoUsuario(XSSFSheet planilha, int numeroLinha) {
-		var linha = planilha.createRow(numeroLinha);
-		adicionarCelula(linha, 0, "Id");
-		adicionarCelula(linha, 1, "Nome");
-		adicionarCelula(linha, 2, "Email");
-		adicionarCelula(linha, 3, "cpf");
-		adicionarCelula(linha, 4, "Tipo");
-	}
-
-	private void adicionarCelula(Row linha, int coluna, String valor) {
-		Cell cell = linha.createCell(coluna);
-		cell.setCellValue(valor);
-	}
-
-	private void adicionarCelula(Row linha, int coluna, int valor) {
-		Cell cell = linha.createCell(coluna);
-		cell.setCellValue(valor);
-	}
-}
